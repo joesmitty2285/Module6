59d272a7e164bd99f7402a17aa155b0731daa45f
joesmitty2285 <joesmitty@me.com>
2024-06-22 19:14:03 -0500

    Final
    Updated Currency Exchange rates

--- CurrencyConverter/CurrencyConverter/CurrencyConverter.swift
+++ CurrencyConverter/CurrencyConverter/CurrencyConverter.swift
@@ -4,10 +4,10 @@ import Foundation
 struct CurrencyConverter {
     static func convert(usd: Int, to currency: String) -> Double {
         let conversionRates = [
-            "EUR": 0.85,
-            "JPY": 110.0,
-            "GBP": 0.75,
-            "CAD": 1.25
+            "EUR": 0.93,
+            "JPY": 159.78,
+            "GBP": 0.79,
+            "CAD": 1.37
         ]
         return Double(usd) * (conversionRates[currency] ?? 1.0)
     }
